eb9e954345c4219b711d4a1987dfcb0c81387d6f
Erik Schierboom <erik_schierboom@hotmail.com>
2023-03-16 15:02:27 +0100

    Detect errors and failures

--- bin/run.sh
+++ bin/run.sh
@@ -45,13 +45,7 @@ sed -i 's/allTestsEnabled = 0/allTestsEnabled = 1/g' "${test_file}"
 # stderr to capture it
 test_output=$(dub test 2>&1)
 exit_code=$?
-
 mv -f "${original_test_file}" "${test_file}" > /dev/null
-
-if [ $exit_code -eq 0 ]; then
-    echo "${test_output}" | grep -q 'All unit tests have been run successfully'
-    exit_code=$?
-fi
 
 popd > /dev/null
 
@@ -71,7 +65,14 @@ else
     colorized_test_output=$(echo "${sanitized_test_output}" \
          | GREP_COLOR='01;31' grep --color=always -E -e '.*(Assertion failure|Error:).*|$')
 
-    jq -n --arg output "${colorized_test_output}" '{version: 1, status: "fail", message: $output}' > ${results_file}
+    echo "${test_output}" | grep -q 'failed with exit code'
+    if [ $? -eq 1 ]; then
+        status="fail"
+    else
+        status="error"
+    fi
+
+    jq -n --arg status "${status}" --arg output "${colorized_test_output}" '{version: 1, status: $status, message: $output}' > ${results_file}
 fi
 
 echo "${slug}: done"
